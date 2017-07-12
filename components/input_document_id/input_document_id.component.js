544d15725c7b660d28f8f28506ddc0cf7c91c2f4
aaeang <aaeang@hotmail.com>
2017-07-12 16:50:16 +0700

    As a developer, I would like to refactor my code by contructing components

---
+++ components/input_document_id/input_document_id.component.js
@@ -0,0 +1,13 @@
+function inputDocumentIdController() {
+    var self = this;
+}
+
+var app = angular.module('scg')
+    .component('inputDocumentId', {
+        templateUrl: '/components/input_document_id/input_document_id.html',
+        bindings: {
+            label: '<',
+            model: '='
+        },
+        controller: function () { }
+    })
