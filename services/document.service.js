bd7ec5e6261d4aec81b2704cba2255034c8a836d
aaeang <aaeang@hotmail.com>
2017-07-14 12:09:20 +0700

    As a developer, I can post httpRequest to actual URL using documentID and user

--- services/document.service.js
+++ services/document.service.js
@@ -4,20 +4,22 @@ var app = angular.module('scg').service('documentService', function ($http) {
 
     self.status = 'initial';
 
-    self.URL = 'http://188.166.210.45:8000/documents/';
+    self.URL = 'http://188.166.210.45:8000/documents/{document_id}?xomlanid={user}';
 
-    self.getDocumentFromAPI = function (documentID) {
+    self.getDocumentFromAPI = function (documentID, user) {
+
+        var DOCUMENT_WITH_USER_URL = self.URL.replace('{document_id}', documentID).replace('{user}', user);
 
         $http({
             method: 'POST',
-            url: self.URL + documentID
+            url: DOCUMENT_WITH_USER_URL
         }).then(function successCallback(response) {
-
+            console.log(response);
             self.status = 'success';
             return response.data;
 
         }, function errorCallback(response) {
-
+            console.log(response);
             self.status = 'failed';
             return response.data;
 
